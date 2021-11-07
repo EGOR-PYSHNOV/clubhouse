13c545b49658bff24861c8d5824e88824fd307a1
Egor Pyshnov <egor475@yahoo.com>
2021-11-07 20:42:16 +0300

    add component room and mock db.json

--- frontend/pages/rooms/[id].tsx
+++ frontend/pages/rooms/[id].tsx
@@ -1,9 +1,12 @@
+import Axios from '../../core/axios'
 import { useRouter } from 'next/router'
+import { GetServerSideProps } from 'next'
 import React from 'react'
 import { BackButton } from '../../components/BackButton'
 import { Header } from '../../components/Header'
+import { Room } from '../../components/Room'
 
-export default function RoomPage() {
+export default function RoomPage({ room }) {
     const router = useRouter()
     const { id } = router.query
 
@@ -14,6 +17,27 @@ export default function RoomPage() {
             <div className='container mt-40'>
                 <BackButton path='/rooms' title='All rooms' />
             </div>
+            <Room title={room.title} />
         </>
     )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    try {
+        const { data } = await Axios.get('/rooms.json')
+        const roomId = ctx.query.id
+        const room = data.find((roomDb) => roomDb.id === roomId)
+        return {
+            props: {
+                room,
+            },
+        }
+    } catch (error) {
+        console.log('ERROR!')
+        return {
+            props: {
+                rooms: [],
+            },
+        }
+    }
 }
