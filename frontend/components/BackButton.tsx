27dd1406bf22d0280a5c13769831505db1223151
EGOR-PYSHNOV <egor475@yahoo.com>
2022-12-03 19:10:54 +0300

    fix format

--- frontend/components/BackButton.tsx
+++ frontend/components/BackButton.tsx
@@ -1,23 +1,19 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
 
 type BackButtonProps = {
-    title: string
-    path: string
-}
+  title: string;
+  path: string;
+};
 
 export const BackButton: React.FC<BackButtonProps> = ({ title, path }) => {
-    return (
-        <Link href={path} passHref>
-            <div className='d-flex mb-30 cup'>
-                <img
-                    src='/static/back-arrow.svg'
-                    alt='Back'
-                    className='mr-10'
-                />
-                <h3>{title}</h3>
-            </div>
-        </Link>
-    )
-}
+  return (
+    <Link href={path} passHref>
+      <div className="d-flex mb-30 cup">
+        <img src="/static/back-arrow.svg" alt="Back" className="mr-10" />
+        <h3>{title}</h3>
+      </div>
+    </Link>
+  );
+};
